a5e127c1df5cda4480178507e9424d7acc34f2dd
Chris <bigcat26@gmail.com>
2025-09-30 21:03:11 +0800

    add osal_posix

--- logger/src/port/osal_posix.c
+++ logger/src/port/osal_posix.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
+
 #ifdef __linux__
 #include <malloc.h>
 #elif __APPLE__
@@ -46,7 +48,26 @@ void logger_lock_free(logger_lock_t **lock) {
 logger_time_t logger_gettime() {
     time_t now = time(NULL);
     struct tm tm;
+    
+#ifdef __APPLE__
+    // macOS 使用 localtime_r 可能有问题，使用 localtime 并复制结构体
+    struct tm *tm_ptr = localtime(&now);
+    if (tm_ptr) {
+        tm = *tm_ptr;
+    } else {
+        // 如果获取时间失败，返回默认时间
+        tm.tm_year = 70;  // 1970
+        tm.tm_mon = 0;    // 1月
+        tm.tm_mday = 1;
+        tm.tm_hour = 0;
+        tm.tm_min = 0;
+        tm.tm_sec = 0;
+    }
+#else
+    // Linux 和其他 POSIX 系统使用 localtime_r
     localtime_r(&now, &tm);
+#endif
+    
     logger_time_t time = {
         .year = tm.tm_year + 1900,
         .month = tm.tm_mon + 1,
@@ -56,4 +77,22 @@ logger_time_t logger_gettime() {
         .second = tm.tm_sec,
     };
     return time;
+}
+
+uint64_t logger_get_timestamp_ns() {
+    struct timespec ts;
+#ifdef __APPLE__
+    // macOS 使用 clock_gettime
+    if (clock_gettime(CLOCK_REALTIME, &ts) == 0) {
+        return (uint64_t)ts.tv_sec * 1000000000ULL + (uint64_t)ts.tv_nsec;
+    }
+#else
+    // Linux 和其他 POSIX 系统
+    if (clock_gettime(CLOCK_REALTIME, &ts) == 0) {
+        return (uint64_t)ts.tv_sec * 1000000000ULL + (uint64_t)ts.tv_nsec;
+    }
+#endif
+    // 如果 clock_gettime 失败，使用 time() 函数
+    time_t now = time(NULL);
+    return (uint64_t)now * 1000000000ULL;
 }
